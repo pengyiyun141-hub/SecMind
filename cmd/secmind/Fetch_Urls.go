413553534084fc669704847754593756bb1293c4
pengyiyun141-hub <pengyiyun141@gmail.com>
2026-05-10 23:11:20 +0800

    error1

--- cmd/secmind/Fetch_Urls.go
+++ cmd/secmind/Fetch_Urls.go
@@ -2,54 +2,49 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"sync"
-	//"io"
-	"log"
 )
 
-func Fetch(urls []string) <- chan Article1 {
+func Fetch(urls []string) <-chan Article1 {
 	var wg sync.WaitGroup
 
 	ch := make(chan Article1, len(urls))
 
-
-	for _, url := range urls{
+	for _, url := range urls {
 		wg.Add(1)
 
-		go func (url string)  {
+		go func(url string) {
 
 			defer wg.Done()
-			
-			
+
 			resp, err := http.Get(url)
 
 			if err != nil {
 				log.Printf("请求失败:[URL]: %s, %s", url, err)
-				return 
+				return
 			}
 
 			defer resp.Body.Close()
 
-			fmt.Println("开始抓取：",url)
-
+			fmt.Println("开始抓取：", url)
 
 			xmlData, err := Parse(resp.Body, url)
 
-
 			if err != nil {
-			log.Printf("请求失败:%s，%s",url,err)
+				log.Printf("请求失败:%s，%s", url, err)
 			}
 
 			if resp.StatusCode != 200 {
-			
+
 				log.Printf("状态码错误: %d", resp.StatusCode)
 			}
 
 			xmlData, err := Parse(resp.Body)
-	
+
 			if err != nil {
-				log.Print("解析失败：",err)
+				log.Print("解析失败：", err)
 			}
 
 			for _, article := range xmlData {
@@ -58,10 +53,9 @@ func Fetch(urls []string) <- chan Article1 {
 		}(url)
 	}
 	go func() {
-    	wg.Wait()   // ① 等待所有 goroutine 完成
-    	close(ch)   // ② 所有任务完成后关闭通道
+		wg.Wait() // ① 等待所有 goroutine 完成
+		close(ch) // ② 所有任务完成后关闭通道
 	}()
 	fmt.Println("3")
 	return ch
 }
-	
